b110500428ea1ea255a3f7b921890ce655379175
sanjith <zuselegacy@gmail.com>
2020-05-18 11:10:39 +0530

    change location of log file to /tmp modify test command to run on mac

--- part1/dotest.sh
+++ part1/dotest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-logfile=~/temp/rlog
+logfile="/tmp/rlog"
 
-go test -v -race -run $@ |& tee ${logfile}
+go test -v -race -run $@ | tee "${logfile}"
 
 go run ../tools/raft-testlog-viz/main.go < ${logfile}
